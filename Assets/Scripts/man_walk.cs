93dcdace933e6c82982487238545f0277b5726bb
aneesha-m <aneeshamathur@ucsb.edu>
2020-03-07 22:52:33 -0800

    God Rays and huge man

---
+++ Assets/Scripts/man_walk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class man_walk : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public GameObject man;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed*Time.deltaTime;
+        man.transform.position += man.transform.forward * step;
+    }
+}
